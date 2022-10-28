2718b430aaa645d78409873a0435f9d642664384
Ileanacz <ileanaguzman@Ileanas-MacBook-Pro.local>
2022-10-28 15:08:24 -0700

    Fixed some code and trying to make something work.

--- script.js
+++ script.js
@@ -1,39 +1,68 @@
 
-function initPage() {
-    var inputEl = document.getElementById("city-input");
-    var searchEl = document.getElementById("search-button");
-    var clearEl = document.getElementById("clear-history");
-    var nameEl = document.getElementById("city-name");
-    var currentPicEl = document.getElementById("current-pic");
-    var currentTempEl = document.getElementById("temperature");
-    var forecastMonth = forecastDateEl.getMonth() + 1;
-    var forecastYear = forecastDateEl.getFullYear();
-    var forecastDateEl = document.createElement("p");
-};
+var inputEl = document.getElementById("city-input");
+var searchEl = document.getElementById("search-button");
+var clearEl = document.getElementById("clear-history");
+var nameEl = document.getElementById("city-name");
+var currentPicEl = document.getElementById("current-pic");
+var currentTempEl = document.getElementById("temperature");
+var forecastDateEl = document.createElement("p");
+var searchBtn = document.getElementById("search-button")
+var date = new Date().toLocaleDateString()
+var cityName = document.getElementById("city-name");
+//var forecastYear = forecastDateEl.getFullYear();
 
 forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
-forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
-forecastEls[i].append(forecastDateEl);
+forecastDateEl.innerHTML = date
+//forecastEls[i].append(forecastDateEl);
 const forecastWeatherEl = document.createElement("img");
 function initPage() {
     renderSearchHistory();
 }
 
-clearEl.addEventListener("click",function() {
-    searchHistory = [];
-    renderSearchHistory();
-})
+// clearEl.addEventListener("click", function () {
+//     searchHistory = [];
+//     renderSearchHistory();
+// })
 
 function findCity() {
-    var cityName = titleCase($("#cityName")[0].value.trim());
+    var cityName = inputEl.value.trim();
 
-var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid8a97f26046812d68e00d0b628569ca1c";
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=8a97f26046812d68e00d0b628569ca1c";
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+           return response.json()
+        }
+    }).then(function (data) {
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+        console.log(data)
+        getForcastWeather(lat,lon)
+        currentWeather(data)
+        // $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
+        // $("#city-list").
+        //     append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
+    })
 }
 
-fetch(apiURL).then(function (response) {
-    if (response.ok) {
-        response.json().then(function (data) ;
-        $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
+function getForcastWeather(lat,lon){
+
+    var apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=8a97f26046812d68e00d0b628569ca1c"
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+           return response.json()
+        }
+    }).then(function (data) {
+       
+        console.log(data)
+        
+        // $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
+        // $("#city-list").
+        //     append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
     })
-        $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
     
+}
+function currentWeather (data){
+    cityName.textContent = data.name
+}
+
+searchBtn.addEventListener("click", findCity)
